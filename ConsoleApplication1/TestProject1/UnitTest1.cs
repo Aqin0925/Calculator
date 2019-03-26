d809459043e2681768337a56fb624f4b0f4cfba8
unknown <2367663268@qq.com>
2019-03-26 17:53:27 +0800

    commit tese!!!!!

---
+++ ConsoleApplication1/TestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+             static void Main(string[] args)
+        {
+            Random a=new Random();
+            Console.WriteLine("请输入你需要的数量");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num1 = a.Next(1, 100);
+                int num2 = a.Next(1, 100);
+                int num3;
+                int num4 = a.Next(0, 5);
+                int num5 = a.Next(1,10);
+                int num6 = a.Next(1,10);
+                int sum;
+                switch (num4)
+                {
+                    case 1: sum = num1 + num2;
+                        Console.WriteLine(num1 + "+" + num2 + "=" + sum);break;
+                    case 2: sum = num1 - num2;
+                        Console.WriteLine(num1 + "-" + num2 + "=" + sum);break;
+                    case 3: sum = num1 * num2;
+                        Console.WriteLine(num1 + "*" + num2 + "=" + sum);break;
+                    case 4: num3 = num5 * num6;
+                        Console.WriteLine(num3 + "/" + num5 + "=" + num6);break;
+                    default: sum = num1 + num2;
+                        Console.WriteLine(num1 + "+" + num2 + "=" + sum); break;
+                }
+            }
+            Console.ReadKey();}
+        }
+    }
+}
